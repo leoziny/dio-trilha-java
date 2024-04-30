7bbced952b850b8784f42cb0d58f94455da9fb4a
leoziny <leonardogomes4261@gmail.com>
2024-04-30 09:39:29 -0300

    Projeto controle de fluxo, DIO

---
+++ ControleFluxo/src/ControleFluxo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ControleFluxo {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        boolean maiorQueSegundo = false;
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidos exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidos {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidos();
+
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int index = 1; index <= contagem; index++) {
+            System.out.println(index);
+        }
+    }
+}
